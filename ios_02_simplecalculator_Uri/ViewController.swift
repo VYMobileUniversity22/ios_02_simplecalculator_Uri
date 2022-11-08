c9fe8d670d7dcbeba1ede51c1029d47297f57657
josep.lopez <josep.lopez@col.vueling.com>
2022-11-08 18:06:12 +0100

    Interface done

--- ios_02_simplecalculator_Uri/ViewController.swift
+++ ios_02_simplecalculator_Uri/ViewController.swift
@@ -8,7 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var btn0: UIButton!
+    @IBOutlet weak var btn3: UIButton!
+    @IBOutlet weak var btn2: UIButton!
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn6: UIButton!
+    @IBOutlet weak var btn5: UIButton!
+    @IBOutlet weak var btn4: UIButton!
+    @IBOutlet weak var btn9: UIButton!
+    @IBOutlet weak var btn8: UIButton!
+    @IBOutlet weak var btn7: UIButton!
+    @IBOutlet weak var btnDecimals: UIButton!
+    @IBOutlet weak var btnIgual: UIButton!
+    @IBOutlet weak var btnMes: UIButton!
+    @IBOutlet weak var btnMenos: UIButton!
+    @IBOutlet weak var btnMultiply: UIButton!
+    @IBOutlet weak var btbDividir: UIButton!
+    @IBOutlet weak var btnTantPercent: UIButton!
+    @IBOutlet weak var btnMasMenos: UIButton!
+    @IBOutlet weak var btnAC: UIButton!
+    @IBOutlet weak var lblDisplay: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
