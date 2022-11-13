ba9f9ef660ca48eaa02efaf90fdc7cd728adf140
josep.lopez <josep.lopez@col.vueling.com>
2022-11-13 14:57:10 +0100

    "refact: cleaning code"
    - Removed the unnecessary code
    - Swimat used

--- ios_02_simplecalculator_Uri/Controller/ViewController.swift
+++ ios_02_simplecalculator_Uri/Controller/ViewController.swift
@@ -8,31 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
 
     @IBOutlet weak var lblDisplay: UITextField!
-    
+
     //var for know if the calculator allready have any value
     let defaults = UserDefaults.standard
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        //btnDecimals.isHidden = defaults.bool(forKey: "decimalsEnabled")     //Hide decimal button by user settings
-        //lblDisplay.text = defaults.string(forKey: "LastResult")             //Display last results
-    }
-//TODO: - setLastResult
-    
-    @IBAction func btnPress(_ sender: UIButton) {
-        CheckIniDisplay(display: lblDisplay, btnPress: sender)
-    }
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-    }
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-    }
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
     }
 }
 
